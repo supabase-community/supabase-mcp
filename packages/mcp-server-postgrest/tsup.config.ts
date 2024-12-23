1f8fe869f191505613e71dc93ec4cd2ca2891e23
Greg Richardson <greg.nmr@gmail.com>
2024-12-23 16:15:12 -0700

    feat: stdio.js for package bin

--- packages/mcp-server-postgrest/tsup.config.ts
+++ packages/mcp-server-postgrest/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig([
   {
-    entry: ['src/index.ts'],
+    entry: ['src/index.ts', 'src/stdio.ts'],
     format: ['cjs', 'esm'],
     outDir: 'dist',
     sourcemap: true,
